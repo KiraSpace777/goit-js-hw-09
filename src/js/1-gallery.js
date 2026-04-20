f700f3034198a36c082023792653d68d8d49b7b5
Anna.K <flysnow@ukr.net>
2026-04-20 22:44:07 +0300

    JS-HW-09, JavaScript (test-1)

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,124 @@
-console.log('Gallery');
+// ---------------------------------------
+//  1 - Масив зображень
+// ---------------------------------------
+
+const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+// ---------------------------------------
+//  2 - Розмітка елементів галереї
+// ---------------------------------------
+
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', createElement(images));
+
+function createElement(arr) {
+  return arr
+    .map(
+      image => `
+      <li class="gallery-item">
+          <a class="gallery-link" href="${image.original}">
+            <img
+               class="gallery-image"
+               src="${image.preview}"
+               alt="${image.description}"
+            />
+          </a>
+        </li>
+  `
+    )
+    .join('');
+}
+
+// ---------------------------------------
+//  3 - модальне вікно галереї
+// ---------------------------------------
+// КРОК 1: ТЕРМИНАЛ - Установка simplelightbox через NPM:
+// npm i simplelightbox
+
+// ------------------------------------------------
+// КРОК 2: javascript -  підключити CSS код бібліотеки simplelightbox
+// ------------------------------------------------
+// підключити CSS, описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// підключити CSS, Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// ------------------------------------------------
+// КРОК 3:
+// Наступний крок це ініціалізація бібліотеки після створення і додання елементів галереї у ul.gallery. Для цього ознайомся з документацією SimpleLightbox — насамперед секції «Usage» і «Markup».
+// ------------------------------------------------
+//-- створюємо екземпляр модального вікна
+// ------------------------------------------------
+const instance = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', // Добавити підпис зображення
+  captionDelay: 250, // Підпис зявляється на зображенні із затримкою на 250 мілісекунд
+  captionPosition: 'bottom', // Підпис зявляється на зображенні знизу
+  close: true, // додається закриття по хрестику (іконка хрестика)
+  showCounter: true, // Каунтер (1/9)
+  enableKeyboard: true, // доступне перелистування через клавіатуру, по стрілкам
+  animationSlide: true, // Эффект горизонтального гортання
+});
